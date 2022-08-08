74660914ef86e3970c7a22c52480172d862b7f8d
Denisse Dominguez Bolaños <80787054+DenisseDB@users.noreply.github.com>
2022-08-08 12:28:20 -0500

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,7 +1,7 @@
 // =================================================================
 //
 // Resaltador de sintaxis paralelo
-// Author: Denisse Dominguez Bolaños-
+// Author: Denisse Dominguez Bolaños
 //
 // =================================================================
 
@@ -19,50 +19,52 @@
 #include <sstream>
 #include <regex>
 
-const int THREADS = 8;
-
 using namespace std;
+
+const int THREADS = 8;
 
 // =========================== SEQUENTIAL ==========================
 string unHilo(string archivo) {
-  fstream pruebas;
-  string resultados, highlight;
+  fstream buscar;
+  string matches, highlight;
   vector<char>::iterator it;
-  pruebas.open(archivo, ios::in);
+  buscar.open(archivo, ios::in);
 
-  while(getline(pruebas, resultados)){
+  while(getline(buscar, matches)){
     // Definimos los operadores aritmeticos
-    string variable = "[a-zA-Z][a-zA-Z_0-9]*";
-    string operadores = "[\\*|\\/|\\^|\\=|\\+|\\-]";
-    string numero = "-*[0-9]+\\.[0-9]+([E][-*][0-9]+)?|-*[0-9]+(\\.[0-9]+)?";
-    //string especiales = "[\\(\\)]";
-    string comentarios = " //.*$"; 
-    string reservada = "main|iostream|#include|using|namespace|std|cout|cin|else|long|switch|break|typedef|return|char|float|const|for|sizeof|if|while|do|int|double|\\/\\/.*|\".*\"|^\\-*[0-9]+| ^[a-zA-Z]+|\\^|\\/|\\&|\\|\\+|\\-|\\*|\\=|\\(|\\)|\\=>|\\<=|.";
-    regex regex_tokens(variable +"|"+ operadores +"|"+ numero +"|"+ comentarios +"|"+ reservada); 
+    string variables = "[a-zA-Z][a-zA-Z0-9_]*";
+    string operadores = "\\/|\\+|\\-|\\*|\\=|\\(|\\)|\\^|\\<<|\\{|\\}|\\>>|\\<=|\\>=|\\;";
+    string comentarios = "\\/\\/.*";
+    string numero = "\\-*[0-9]+";
+    string reservada = "#include|iostream|cin|printf|auto|else|endl|switch|break|enum|cout|typedef|string|extern|return|union|char|float|using|namespace|const|for|main|void|continue|goto|sizeof|volatile|default|if|stati|while|do|int|struct_Packed|double";
+    string strings = "\".*\"";
+    regex regex_tokens(comentarios +"|"+ strings +"|"+ reservada +"|"+ numero +"|"+ reservada +"|"+ variables  +"|"+ operadores + "|.");
 
     // Definimos nuestro iterador para ir recorriendo y buscar en regex los tokens
     // Iterador para ir recorriendo match por match y detectarlos en regex_tokens
-    auto inicio_arch = 
-    // leer de resultados incio a resultados final 
-    std::sregex_iterator(resultados.begin(), resultados.end(), regex_tokens); 
-    // cuando ya no tenga mas elementos se deja de leer 
-    auto fin_arch = std::sregex_iterator(); 
+    auto inicio_archivo =
+    // leer de resultados incio a resultados final
+    std::sregex_iterator(matches.begin(), matches.end(), regex_tokens);
+    // cuando ya no tenga mas elementos se deja de leer
+    auto fin_archivo = std::sregex_iterator();
 
     // Vamos a ir leyendo caracter x caracter, al detectar su token, pasamos al siguiente carcater
-    for (std::sregex_iterator i = inicio_arch; i != fin_arch; ++i) {
-      std::smatch match = *i;                                              
-      std::string caracter = match.str(); 
+    for (std::sregex_iterator i = inicio_archivo; i != fin_archivo; ++i) {
+      std::smatch match = *i;
+      std::string caracter = match.str();
       // Ya que tebemos el string, vemos si esta hace match con el regex, que basta solo con poner regex ya que se llama al constructor
-      if(regex_match(caracter, regex(variable))){ 
-        highlight = highlight + "<span class=\"variable\">" + caracter + "</span>";
-      }else if(regex_match(caracter, regex(numero))){
-        highlight = highlight + "<span class=\"numero\">" + caracter + "</span>";
-      }else if(regex_match(caracter, regex(comentarios))){
+      if(regex_match(caracter, regex(comentarios))){
         highlight = highlight + "<span class=\"comentario\">" + caracter + "</span>";
-      }else if(regex_match(caracter, regex(operadores))){
-        highlight = highlight + "<span class=\"operador\">" + caracter + "</span>";
       }else if(regex_match(caracter, regex(reservada))){
         highlight = highlight + "<span class=\"reservada\">" + caracter + "</span>";
+      }else if(regex_match(caracter, regex(numero))){
+        highlight = highlight  + "<span class=\"numero\">" + caracter + "</span>";
+      }else if(regex_match(caracter, regex(variables))){
+        highlight = highlight + "<span class=\"variable\">" + caracter + "</span>";
+      }else if(regex_match(caracter, regex(operadores))){
+        highlight = highlight + "<span class=\"operador\">" + caracter  + "</span>";
+      }else{
+        highlight = highlight + "<span class=\"string\">" + caracter  + "</span>";
       }
     }
   }
@@ -71,17 +73,86 @@ string unHilo(string archivo) {
 
 // =================================================================
 
+
 // =========================== CONCURRENT ==========================
+//ifstream para leer entradas y sacar cosas
 typedef struct {
-    string *array;
+    string file;
 } Block;
 
 void* variosHilos (void* param) {
-  Block *block;
+    const string htmlStart = "<!DOCTYPE html><html lang='esp-mx'> <head> <meta charset='UTF-8'> <link rel=\"stylesheet\" href=\"./styles_AS.css\"> <title>Resaltador de Sintaxis Paralelo </title></head><body>";
+    const string htmlEnd = "</body></html>";
 
-  block = (Block*) param;
-  for (int i = block->start; i < block->end; i++) {
-    
+    Block *block;
+    block = (Block*) param;
+
+    string result = htmlStart + unHilo(block->file) + htmlEnd;
+
+    ofstream archivo;
+    archivo.open("ResultadoParalelo-" + block->file + ".html");
+    archivo << result;
+    archivo.close();
+
+    pthread_exit(0);
+}
+
+// ===========================================================
+
+// ===========================================================
+int main(int argc, char* argv[]) {
+  const string htmlStart = "<!DOCTYPE html><html lang='esp-mx'> <head> <meta charset='UTF-8'> <link rel=\"stylesheet\" href=\"./styles_AS.css\"> <title>Resaltador de Sintaxis Paralelo </title></head><body>";
+  const string htmlEnd = "</body></html>";
+
+  //argv es un arreglo de strings
+  double tiempoSecuencial, tiempoConcurrente;
+  int blockSize;
+
+  // argc es la cantidad de elementos a recibir
+  // por lo tonto lo consideramos el tamaño de
+  // entrada
+  int tamanoEntrada = argc;
+
+  // ========= HTML SEQUENTIAL ==============
+  tiempoSecuencial = 0;
+  start_timer();
+
+  for (int i = 1; i < argc; i++) {
+    string result = htmlStart + unHilo(argv[i]) + htmlEnd;
+
+    // realizamos el archivo de salida
+    // lo generamos con ofstream
+    ofstream archivo;
+    archivo.open("ResultadoSecuencial-" + string(argv[i]) + ".html");
+    archivo << result;
+    archivo.close();
+
   }
-  pthread_exit(0);
-} 
+  tiempoSecuencial = stop_timer();
+
+  cout << "Tiempo en secuencial = " << setprecision(5) << (tiempoSecuencial) <<" ms" << endl;
+
+  // ========= CONCURRENT ==========
+  tiempoConcurrente = 0;
+  start_timer();
+
+  pthread_t tids[argc];
+  Block blocks[argc];
+
+  for (int i = 1; i < argc; i++) {
+    blocks[i].file = argv[i];
+  }
+
+  for (int i = 1; i < argc; i++) {
+     pthread_create(&tids[i], NULL, variosHilos, (void*) &blocks[i]);
+   }
+
+ for (int i = 1; i < argc; i++) {
+   pthread_join(tids[i], NULL);
+ }
+
+  tiempoConcurrente = stop_timer();
+  cout << "Tiempo en paralelo = " << setprecision(5) << (tiempoConcurrente) <<" ms" << endl;
+
+  return 0;
+}
